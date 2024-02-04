6502fe935ba1f93efb079eee605c5e433954dedf
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-04 21:46:22 +0800

    西元2024年02月04日 (週日) 21時46分22秒 CST

--- startup.bat
+++ startup.bat
@@ -3,6 +3,6 @@ REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "ht
 REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "C:\Users\pulip\Downloads\Fullscreen Qual Webcam Viewer.html"
 
 start "" "C:\Users\pulip\Downloads\x86_64-pc-windows-msvc-simple-http-server.exe"
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "http://localhost:8000/Fullscreen-Quad-Webcam-Viewer-main/index.html#/"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "http://localhost:8000/Fullscreen-Quad-Webcam-Viewer/index.html#/"
 
 exit

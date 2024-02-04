94c799ee3f074b619f8c91ce17073c65fc7c8b12
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-04 21:15:27 +0800

    西元2024年02月04日 (週日) 21時15分27秒 CST

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A PWA to display four webcams in a webpage.
 
 https://pulipulichen.github.io/Fullscreen-Quad-Webcam-Viewer/
+
+
+https://git-scm.com/download/win
+
+https://github.com/TheWaWaR/simple-http-server/releases
---
+++ startup.bat
@@ -0,0 +1,8 @@
+@echo off
+REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "https://pulipulichen.github.io/Fullscreen-Quad-Webcam-Viewer/#/"
+REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "C:\Users\pulip\Downloads\Fullscreen Qual Webcam Viewer.html"
+
+start "" "C:\Users\pulip\Downloads\x86_64-pc-windows-msvc-simple-http-server.exe"
+REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "http://localhost:8000/Fullscreen-Quad-Webcam-Viewer-main/index.html#/"
+
+exit

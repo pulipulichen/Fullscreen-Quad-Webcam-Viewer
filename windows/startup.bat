0045273c050e71aea4136e36b5200218ee9c0385
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-05 15:30:47 +0800

    西元2024年02月05日 (週一) 15時30分47秒 CST

--- windows/startup.bat
+++ windows/startup.bat
@@ -2,6 +2,8 @@
 REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "https://pulipulichen.github.io/Fullscreen-Quad-Webcam-Viewer/#/"
 REM start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "C:\Users\pulip\Downloads\Fullscreen Qual Webcam Viewer.html"
 
+pushd "%~dp0.." & rem Change working directory to the parent folder
+
 start "" "C:\Users\pulip\Downloads\x86_64-pc-windows-msvc-simple-http-server.exe"
 timeout /t 3 /nobreak > nul
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk "http://localhost:8000/Fullscreen-Quad-Webcam-Viewer/index.html"

ba14b54cf24af591574dc975cb566ede81072514
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-05 15:25:58 +0800

    西元2024年02月05日 (週一) 15時25分58秒 CST

---
+++ windows/update.bat
@@ -0,0 +1,7 @@
+@echo off
+pushd "%~dp0.." & rem Change working directory to the parent folder
+echo Current directory: %CD%
+rem Your script's commands go here
+popd & rem Restore the original working directory
+
+git pull

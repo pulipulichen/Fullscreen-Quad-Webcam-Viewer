89481a1d17dbd27a4e5b558057a9cbea201e282c
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-06-01 03:31:45 +0800

    西元2025年06月01日 (週日) 03時31分45秒 CST

---
+++ linux/vlc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 偵測如果沒有vlc，則安裝
+if ! command -v vlc &> /dev/null
+then
+    echo "VLC not found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y vlc
+else
+    echo "VLC is already installed."
+fi
+
+# 啟動vlc api
+vlc --intf http --http-port 8080 --daemon
